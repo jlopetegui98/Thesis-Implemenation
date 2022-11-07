2a199980db741fcb809f4a4c676d81b5dd32a368
jlopetegui98 <javier.lopetegui@estudiantes.matcom.uh.cu>
2022-11-07 07:30:58 -0500

    update: sentence dataset modification to return embedding for dataLoader

--- content_model_pytorch/data_loader.py
+++ content_model_pytorch/data_loader.py
@@ -1,20 +1,23 @@
 from .bow_model.bow_model import Vocab
 from torch.utils.data import Dataset
+import torch
 
 # class for dataset of sentences used to train content model
 class SentencesDataset(Dataset):
     def __init__(
         self,
-        sentences_embeddings,
+        sentences_embeddings_path,
         content_words_sentences,
         mode="train",
         word2indx=None,
+        batches_size=100,
     ):
-        self.sentences = sentences_embeddings
-        self.content_words_sentences = content_words_sentences
+        self.sentences_embeddings_path = sentences_embeddings_path
+        self.content_words_sentencs = content_words_sentences
         self.mode = mode
         self.word2indx = word2indx
         self.vocab = self._get_vocab()
+        self.batch_size = batches_size
 
     # create Vocab instance from content words sentences
     def _get_vocab(self):
@@ -29,7 +32,11 @@ class SentencesDataset(Dataset):
         return sentence_embedding
         return bow representation of sentence's content
         """
-
-        return self.sentences[index], self.vocab.get_bow_representation(
+        bath_idx = index // self.batch_size
+        sent_idx = index % self.batch_size
+        sentence_embedding = torch.load(
+            f"{self.sentences_embeddings_path}{self.mode}_batch_{bath_idx+1}.pt"
+        )[sent_idx]
+        return sentence_embedding, self.vocab.get_bow_representation(
             self.content_words_sentences[index]
         )
